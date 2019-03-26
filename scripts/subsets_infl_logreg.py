752a240bc45ae80a4c495cdf3d52a0bea9a88a05
Hubert Teo <hubert.teo.hk@gmail.com>
2019-03-26 02:34:13 -0700

    Add dataset directory argument to existing experiment scripts

--- scripts/subsets_infl_logreg.py
+++ scripts/subsets_infl_logreg.py
@@ -10,6 +10,8 @@ from experiments.subset_influence import SubsetInfluenceLogreg
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compute subset self and test influences')
     # Environment args
+    parser.add_argument('--data-dir', default=None, type=str,
+                        help="The base dataset directory")
     parser.add_argument('--out-dir', default=None, type=str,
                         help="The experiment output directory")
     parser.add_argument('--max-memory', default=1e9, type=int,
@@ -37,6 +39,7 @@ if __name__ == "__main__":
         'dataset_id': args.dataset_id,
         'center_data': False,
         'append_bias': False,
+        'data_dir': args.data_dir,
     }
     config = {
         'dataset_config': dataset_config,
